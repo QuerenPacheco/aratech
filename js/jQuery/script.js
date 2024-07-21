d73d0755dd5d6d8b4cd85861cf9d42c70a09aeda
Queren <querenpachecocastro@gmail.com>
2024-07-21 13:59:00 +0200

    add styles to jquery exercise

--- js/jQuery/script.js
+++ js/jQuery/script.js
@@ -4,34 +4,34 @@ $(document).ready(function () {
 });
 
 function checkTasksList() {
-    if ($('#tasksList').html().trim() === '') {
-        $('#tasksList').html('<p id="noContentMsg" class="p-2 mb-2">No hay tareas pendientes</p>');
+    if ($('#taskTable tbody').html().trim() === '') {
+        $('.container').append('<p id="noContentMsg">No hay tareas pendientes</p>');
+        $('#taskTable').hide();
+
     }
 }
 
 function addTask() {
-    let newTask = $("#title").val();
+    const newTask = $("#title").val().trim();
 
-    if (newTask.trim().length > 0) {
-        if ($('#noContentMsg').length) {
-            $('#noContentMsg').remove()
-        }
+    if (newTask.length) {
+        $("#errorMsg").empty();
+        $('#noContentMsg').remove();
+        $('#taskTable').show();
+        const row = $('<tr></tr>');
+        const task = $(`<td class="task p-2 mb-2">${newTask}</td>`);
 
-        $('#noContentMsg').remove()
-        let task = $(`<p class="task p-2 mb-2">${newTask}</p>`)
-
-        let removeButton = $('<button class="btn btn-danger btn-sm">Eliminar tarea</button>');
+        const removeButton = $('<td><button class="btn btn-danger btn-sm">Eliminar</button></td>');
         removeButton.click(function () {
             $(this).parent().remove();
             checkTasksList();
         });
-
-        task.append(removeButton);
-        $('#tasksList').append(task);
+        row.append(task).append(removeButton);
+        $('#taskTable tbody').append(row);
         $("#title").val('');
     } else {
-        let message = "Debe rellenar el campo";
-        $("#errorMsg").html('<div class="alert alert-danger" role="alert">' + message + '</div>')
+        const message = "Debe rellenar el campo";
+        $("#errorMsg").html('<div class="alert alert-danger" role="alert">' + message + '</div>');
     }
 
 }
